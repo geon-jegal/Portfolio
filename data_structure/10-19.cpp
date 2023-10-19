0f76a7135d2b402b6772f16ccb68b186dc1326b2
henac <man25816@naver.com>
2023-10-19 17:56:51 +0900

    과제 완료

--- data_structure/10-19.cpp
+++ data_structure/10-19.cpp
@@ -23,13 +23,6 @@ BSTNode* CreateNode(int data) {
 	return newNode;
 }
 
-// 부모 노드와 좌우 자식 노드를 설정합니다.
-void MakeTree(BSTNode* parent, BSTNode* lson, BSTNode* rson)
-{
-	parent->left = lson;
-	parent->right = rson;
-}
-
 // 새로운 노드를 생성하고 초기화합니다.
 BSTNode* newNode(int item) {
 	BSTNode* newNode = (BSTNode*)malloc(sizeof(BSTNode));
@@ -61,24 +54,9 @@ BSTNode* insert_node(BSTNode* root, int key) {
 	return root;
 }
 
-BSTNode* min_value_node(BSTNode* node) {
-	BSTNode* current = node;
-
-	//맨 왼쪽 단말 노드를 찾으러 내려감
-	while (current->left != NULL) current = current->left;
-
-	return current;
-}
-
 BSTNode* FindMin(BSTNode* node) {
 	while (node->left != NULL)
 		node = node->left;
-	return node;
-}
-
-BSTNode* FindMax(BSTNode* node) {
-	while (node->right != NULL)
-		node = node->right;
 	return node;
 }
 
@@ -119,64 +97,6 @@ BSTNode* Delete_Node(BSTNode* root, int data)
 		}
 	}
 	return root;
-}
-
-
-//순환적인 탐색 함수
-BSTNode* search(BSTNode* node, int key) {
-	if (node == NULL) {
-		printf("데이터를 검색하지 못 하였습니다.\n");
-		return NULL;
-	}
-	if (key == node->data) {
-		printf("%d", node->data);
-		return node; // (1)
-	}
-	else if (key < node->data)
-		return search(node->left, key); // (2)
-	else
-		return search(node->right, key); // (3)
-}
-
-int search_count_recursive(BSTNode* node, int key, int count) {
-	if (node == NULL) {
-		return count; // 데이터를 찾지 못한 경우 현재까지의 탐색 횟수 반환
-	}
-
-	count++;
-
-	if (key == node->data) {
-		return count;
-	}
-	else if (key < node->data) {
-		return search_count_recursive(node->left, key, count);
-	}
-	else {
-		return search_count_recursive(node->right, key, count);
-	}
-}
-
-// 반복적인 탐색 함수
-BSTNode* search_Iter(BSTNode* node, int key) {
-	int level = 0;
-	while (node != NULL) {
-		if (key == node->data) {
-			level++;
-			printf("\n트리의 검색 횟수(iter) : %d\n", level);
-			return node;
-		}
-		else if (key < node->data) {
-			node = node->left;
-			level++;
-		}
-		else {
-			node = node->right;
-			level++;
-		}
-	}
-	printf("데이터를 검색하지 못 하였습니다.\n");
-	printf("트리의 검색 횟수(iter) : %d\n", level);
-	return NULL; // 탐색에 실패했을 경우 NULL 반환
 }
 
 BSTNode* randomTree_BST(int num) {
@@ -247,50 +167,6 @@ void inorder_traversal(BSTNode* node) {
 	inorder_traversal(node->right);
 }
 
-void preorder_traversal(BSTNode* node) {
-	if (node == NULL) return;
-
-	printf("%d ", node->data);
-	preorder_traversal(node->left);
-	preorder_traversal(node->right);
-}
-
-// 노드의 수를 세는 함수
-int count_nodes(BSTNode* node) {
-	if (node == NULL) return 0;
-	return 1 + count_nodes(node->left) + count_nodes(node->right);
-}
-
-// 트리 깊이 계산 함수
-int tree_depth(BSTNode* node) {
-	if (node == NULL) return -1; // 빈 트리는 -1을 반환
-	int left_depth = 0;
-	left_depth = tree_depth(node->left);
-	int right_depth = 0;
-	right_depth = tree_depth(node->right);
-	return 1 + (left_depth > right_depth ? left_depth : right_depth);
-}
-
-
-void print_node(BSTNode* node) {
-	if (node == NULL) return;
-
-	printf("%d ", node->data);
-
-	if (node->left != NULL || node->right != NULL) {
-		printf("(");
-		if (node->left != NULL) print_node(node->left);
-		else printf("NULL");
-
-		printf(", ");
-
-		if (node->right != NULL) print_node(node->right);
-		else printf("NULL");
-
-		printf(")");
-	}
-}
-
 BSTNode* Delete(BSTNode* q) {
 	BSTNode* node = (BSTNode*)malloc(sizeof(BSTNode));
 	node->data = FindMin(q)->data;
@@ -322,17 +198,22 @@ int main(void) {
 	for (int i = 0; i < 1000; i++) {
 		int r = rand() % 100000; // 랜덤한 값을 생성
 		q[i] = r;
-		printf("[%d], ", q[i]);
+		// printf("[%d], ", q[i]);
 	}
-	int min = 100000;
+	int min = 100000, pos = 0;
+	// 최솟값 찾기
 	for (int i = 0; i < 1000; i++) {
-		if (min > q[i]) min = q[i];
+		if (min > q[i]) {
+			min = q[i];
+			pos = i;
+		}
 	}
-	for (int i = 0; i < 1000; i++) {
-		if (min == q[i]) q[i] = NULL;
+	printf("우선순위에 따라 삭제한 값: [%d], 위치: %d", min, pos);
+	q[pos] = NULL;
+	for (int i = pos + 1; i < 1000; i++) {
+		q[i - 1] = q[i];
 	}
-	printf("\n\n우선순위에 따라 삭제한 값: [%d] ", min);
-
+	printf("\nPriority Queue Delete작업 후 다시 정렬\n");
 
 	// 순서있는 배열
 	int q2[1000] = {};
@@ -342,28 +223,33 @@ int main(void) {
 		//printf("[%d], ", q2[i]);
 	}
 	Sort(q2, 1000);
-	printf("\n정렬\n");
+	printf("\n\n정렬(순서 있는 배열)\n");
 	for (int i = 0; i < 1000; i++) {
-		printf("[%d], ", q2[i]);
+		//printf("[%d], ", q2[i]);
 	}
-	printf("\n\n우선순위에 따라 삭제한 값: [%d] ", q2[0]);
+	printf("우선순위에 따라 삭제한 값: [%d] ", q2[0]);
 	q2[0] = NULL;
+	for (int i = 1; i < 1000; i++) {
+		q2[i - 1] = q2[i];
+	}
+	printf("\nPriority Queue Delete작업 후 다시 정렬\n");
 
 	// BST
+	printf("\nPriority Queue (BST)");
 	BSTNode* root_randBST = NULL;
 
-	root_randBST = randomTree_BST(10);
-	printf("\ninorder\n");
-	inorder_traversal(root_randBST);
-	printf("\nlevel order\n");
-	level_order(root_randBST);
+	root_randBST = randomTree_BST(1000);
+	//printf("\ninorder\n");
+	//inorder_traversal(root_randBST);
+	//printf("\nlevel order\n");
+	//level_order(root_randBST);
 
 	Delete(root_randBST);
 
-	printf("\ninorder\n");
-	inorder_traversal(root_randBST);
-	printf("\nlevel order\n");
-	level_order(root_randBST);
+	//printf("\ninorder\n");
+	//inorder_traversal(root_randBST);
+	//printf("\nlevel order\n");
+	//level_order(root_randBST);
 
 	return 0;
 }
